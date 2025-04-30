034b3ee885d8c040b67c51e9c10dc8b25615e5a6
Devansh <devanshagarwal788@gmail.com>
2025-05-01 02:56:18 +0530

    feat: add ProtectedRoute component to secure chat access for logged-in users

--- spotify-frontend/src/App.jsx
+++ spotify-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LoginSignup from "./components/LoginSignup";
 import { useContext } from "react";
 import Display from "./components/Display";
@@ -13,6 +13,18 @@ function App() {
   const { audioRef, track, songsData } = useContext(PlayerContext);
   const { user } = useContext(UserContext);
 
+  function ProtectedRoute({ children }) {
+    const { user } = useContext(UserContext);
+
+    // If the user is not logged in, redirect to the login page
+    if (!user) {
+      return <Navigate to="/login" replace />;
+    }
+
+    // If the user is logged in, render the children components
+    return children;
+  }
+
   return (
     <div className="h-screen flex flex-col bg-black text-white">
       <Routes>
@@ -26,7 +38,14 @@ function App() {
                   <Routes>
                     <Route path="*" element={<Display />} />
                     <Route path="/search" element={<SongSearch />} />
-                    <Route path="/chat" element={<ChatDashboard />} />
+                    <Route
+                      path="/chat"
+                      element={
+                        <ProtectedRoute>
+                          <ChatDashboard />
+                        </ProtectedRoute>
+                      }
+                    />
                   </Routes>
                 </div>
                 <Player />
@@ -38,7 +57,11 @@ function App() {
       </Routes>
 
       {/* Add the audio element */}
-      <audio ref={audioRef} src={track ? track.file : ""} preload="none"></audio>
+      <audio
+        ref={audioRef}
+        src={track ? track.file : ""}
+        preload="none"
+      ></audio>
     </div>
   );
 }
